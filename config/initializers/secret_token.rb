d994b21f29229074c13c2a0d6d975da0135d1226
sumit <sumit@sumit-HP-Pavilion-g6-Notebook-PC.(none)>
2014-03-23 16:49:56 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dinesh::Application.config.secret_token = '0c74b9f9307bf73b1ec0453fd5cb7b64acb79646b0ce247d41d27fceedffbe0d6974787fb7ed2029225cf1b6fd401a4992bf63675f42de3a5956c1cd36dd8244'
